262d712774040f3d5279fc29be17f3b0cf1ae6a1
rda-cunh <ricard.mendes@gmail.com>
2024-10-28 18:32:10 +0000

    project structure

---
+++ philo/inc/philo.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 18:08:06 by rda-cunh          #+#    #+#             */
+/*   Updated: 2024/10/28 18:22:20 by rda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h> //printf
+# include <stdlib.h> //memory allocation
+# include <string.h> //memset
+# include <unistd.h> //write  and usleep
+# include <sys/time.h> //gettimeoftheday
+# include <pthread.h> //thread functions
+
+//struct
+typedef struct  s_philo
+{
+
+}   t_philo; 
+
+typedef struct  s_data
+{
+    /* data */
+}   t_data;
+
+
+//functions
+
+
+#endif
---
+++ philo/src/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 18:08:22 by rda-cunh          #+#    #+#             */
+/*   Updated: 2024/10/28 18:29:40 by rda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+int main(int argc, char **argv)
+{
+    int number_of_philosophers; //and number of forks
+    int time_to_die; //time do die since last meal or beggining of simulation
+    int time_to_eat; //time each philosopher needs to eat
+    int time_to_sleep; //time each philosopher will spend sleeping
+    int number_of_times_each_philosopher_must_eat; //simulation stops if all philosophers eat this number of times
+
+    if (argc == 5 || argc == 6)
+    {
+        //read arguments and atoi them
+        //store variables in a structure
+    }
+    return (0); 
+}
