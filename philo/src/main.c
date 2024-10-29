0dcb2506374b6e4b4249fbcd32811412a2661f28
rda-cunh <ricard.mendes@gmail.com>
2024-10-29 00:54:24 +0000

    added pseudio code structure

--- philo/src/main.c
+++ philo/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 18:08:22 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/10/28 18:29:40 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/10/29 00:53:44 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,20 @@ int main(int argc, char **argv)
     if (argc == 5 || argc == 6)
     {
         //read arguments and atoi them
-        //store variables in a structure
+        //store variables in a structure (t_info)
+
+        //initialize philosophers as threads (usind a t_philo struct) [don't forget to assign them ids (i+1), forks and staarting times]
+        //initialize mutexes to control the access to each fork and associate each philosoper with two adjacent threads
+        
+        //initialize the simulation loop:
+            // 1. Eating: each philosopher locks two mutexes (forks) and simulates eating for time_to_eat milliseconds
+            // 2. Sleeping: each philosopher "puts down" both forks (unlocks mutexes) and sleeps for time_to_sleep milliseconds
+            // 3. Thinking: the philosopher transitions to thinking, ensuring they check their time_to_die threshold
+        //track each philosopher last meal time. After each action, check if the philosopher's time without eating exceeds time_to_die. If so, log their death and terminate simulation
+
+        //Must have a function to keep the log and print alls the timestamps ans messages (Synchronize logging (e.g. mutex) to avoid log message overlap, ensuring a clear output
+        //clean um functions that destoy_mutexes and free_resources
+
     }
     return (0); 
 }
