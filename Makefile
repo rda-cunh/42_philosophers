623f37d6d301b10ce1246481096f2a602aa12988
rda-cunh <ricard.mendes@gmail.com>
2024-10-30 00:08:38 +0000

    updated Makefine and some ideas for struct

--- Makefile
+++ Makefile
@@ -6,7 +6,34 @@
 #    By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/28 23:22:38 by rda-cunh          #+#    #+#              #
-#    Updated: 2024/10/28 23:24:29 by rda-cunh         ###   ########.fr        #
+#    Updated: 2024/10/29 23:29:25 by rda-cunh         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = philo
+
+SRC_DIR = src
+SRCS = $(SRC_DIR)/main.c		 
+OBJS = $(SRCS:.c=.o)
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -pthread
+RM = rm -rf
+
+all: $(NAME)
+
+$(NAME): $(OBJS) 
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+clean:
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 18:08:06 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/10/29 00:37:01 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/10/30 00:07:37 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,24 @@
 //struct
 typedef struct  s_philo
 {
-
+    //ID
+    //Flags for eating, thinking or sleeping
+    //is_alive flag (check if he died)
+    //last_meal_time (timestamp in miliseconds) to record the time from the last time philo ate
+    //left fort and right fort (pointers) to mutex forks the philo controls
+    //eat_count to count the number of meals eated and control las program argument; 
 }   t_philo; 
 
 typedef struct  s_info
 {
-    /* data */
+    // number_of_philosophers: the count of philosophers.
+    // time_to_die: time limit before a philosopher starves if they don’t eat.
+    // time_to_eat: duration of each eating session.
+    // time_to_sleep: duration of each sleeping session.
+    // meals_required (optional): the number of times each philosopher should eat, if it’s a stopping condition.
+    // the forks (array of mutex w/ pthread_mutex_t)
+    // start time timestamp (begginning of simulations)
+    // an array of t_philo to track the philosophers
 }   t_info;
 
 
