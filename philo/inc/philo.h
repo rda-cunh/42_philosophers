c0cb2ad7788c6bb18ff016bc6d0e7d24013ecb70
rda-cunh <ricard.mendes@gmail.com>
2024-11-08 18:43:27 +0000

    added structs and initialization

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 18:08:06 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/10/31 16:25:49 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/11/08 18:36:07 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,43 @@
 # include <sys/time.h> //gettimeoftheday
 # include <pthread.h> //thread functions
 
-//struct
+//structs (must check during implementation!)
 typedef struct  s_philo
 {
-    //ID
-    //Flags for eating, thinking or sleeping
-    //is_alive flag (check if he died)
-    //last_meal_time (timestamp in miliseconds) to record the time from the last time philo ate
-    //left fort and right fort (pointers) to mutex forks the philo controls
-    //eat_count to count the number of meals eated and control las program argument; 
+	pthread_t   	thread; //thread
+	int         	philo_id; //philo ID 
+	int 			flg_is_alive; //flag to sinalize of philo died (not shure if needed!)
+	int				eat_count; //count the number of meals eated and control las program argument (when given)
+	long long		time_meal; //time from last meal (to check if he died)
+	pthread_mutex_t	*right_fork;  //right fork (pointer) to mutex the philo controls
+	pthread_mutex_t *left_fork; //left fork (pointer) to mutex the philo controls
+	t_table			*table; //pointer to access table struct
 }   t_philo; 
 
 typedef struct  s_table
 {
-    // number_of_philosophers: the count of philosophers.
-    // time_to_die: time limit before a philosopher starves if they don’t eat.
-    // time_to_eat: duration of each eating session.
-    // time_to_sleep: duration of each sleeping session.
-    // meals_required (optional): the number of times each philosopher should eat, if it’s a stopping condition.
-    // the forks (array of mutex w/ pthread_mutex_t)
-    // start time timestamp (begginning of simulations)
-    // an array of t_philo to track the philosophers
+	unsigned int	num_philo; //number_of_philosophers: the count of philosophers
+	long long		time_die; //time_to_die: time limit before a philosopher starves if they don’t eat.
+	long long		time_eat; //time_to_eat: duration of each eating session
+	long long		time_sleep; //time_to_sleep: duration of each sleeping session
+	unsigned int	meals_required; //meals_required (optional): the number of times each philosopher should eat, if it’s a stopping condition.
+	int				end_meal_flg; //flag that signals the end of the simulation (a philo died or meals required are achieved)
+    long long		start_time; //start time timestamp (begginning of simulations)
+	t_philo			*philos; //an array of t_philo to track the philosophers
+	pthread_mutex_t *forks; //an array to the store the mutexes referenced in my philo struct
 }   t_table;
 
 
-//functions
+//funtions
+//main.c
+void	set_table(t_table *table, int argc, char **argv);
+void	summon_philos(t_table s_table); 
+
+//utils.c
+int		ft_atoi(const char *str); 
+
+//exit.c
+int		error_exit(char *error_msg); 
 
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,43 +6,52 @@
 /*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 18:08:22 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/10/31 16:30:29 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/11/08 18:42:38 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void	set_table(int argc, char **argv)
+void	summon_philos(t_table *s_table)
 {
-	int number_of_philosophers; //and number of forks
-    int time_to_die; //time do die since last meal or beggining of simulation
-    int time_to_eat; //time each philosopher needs to eat
-    int time_to_sleep; //time each philosopher will spend sleeping
-    int number_of_times_each_philosopher_must_eat; //simulation stops if all philosophers eat this number of times
+	//initialize philosophers as threads (usind a t_philo struct) [don't forget to assign them ids (i+1), forks and staarting times]
+}
 
-	if (argc != 5 ||
-
+void	set_table(t_table *table, int argc, char **argv)
+{
+    if (argc != 5 || argc != 6)
+        error_exit("Wrong number of arguments");
+    table->num_philo = ft_atoi(argv[1]);
+	table->time_die = ft_atoi(argv[2]);
+	table->time_eat = ft_atoi(argv[3]);
+	table->time_sleep = ft_atoi(argv[4]);
+	if (argv == 6)
+		table->meals_required = ft_atoi(argv[5]);
+	else
+		table->meals_required = -1;
+	table->end_meal_flg = 0;
+	if (table->num_philo < 1 || table->time_die < 1 || table->time_eat < 1 \
+	|| table->time_sleep < 1 || (argc == 6 && table->meals_required < 1))
+		error_exit("Invalid arguments"); 
 }
 
 int main(int argc, char **argv)
 {
-	set_table(argc, argv); 
+    t_table	table; 
 
+	set_table(&table, argc, argv);
+	summon_philos(&table);
 
-        //read arguments and atoi them
-        //store variables in a structure (t_info)
-
-        //initialize philosophers as threads (usind a t_philo struct) [don't forget to assign them ids (i+1), forks and staarting times]
-        //initialize mutexes to control the access to each fork and associate each philosoper with two adjacent threads
+//initialize mutexes to control the access to each fork and associate each philosoper with two adjacent threads
         
-        //initialize the simulation loop:
-            // 1. Eating: each philosopher locks two mutexes (forks) and simulates eating for time_to_eat milliseconds
-            // 2. Sleeping: each philosopher "puts down" both forks (unlocks mutexes) and sleeps for time_to_sleep milliseconds
-            // 3. Thinking: the philosopher transitions to thinking, ensuring they check their time_to_die threshold
-        //track each philosopher last meal time. After each action, check if the philosopher's time without eating exceeds time_to_die. If so, log their death and terminate simulation
+//initialize the simulation loop:
+    // 1. Eating: each philosopher locks two mutexes (forks) and simulates eating for time_to_eat milliseconds
+    // 2. Sleeping: each philosopher "puts down" both forks (unlocks mutexes) and sleeps for time_to_sleep milliseconds
+    // 3. Thinking: the philosopher transitions to thinking, ensuring they check their time_to_die threshold
+    //track each philosopher last meal time. After each action, check if the philosopher's time without eating exceeds time_to_die. If so, log their death and terminate simulation
 
-        //Must have a function to keep the log and print alls the timestamps ans messages (Synchronize logging (e.g. mutex) to avoid log message overlap, ensuring a clear output
-        //clean um functions that destoy_mutexes and free_resources
+//Must have a function to keep the log and print alls the timestamps ans messages (Synchronize logging (e.g. mutex) to avoid log message overlap, ensuring a clear output
+//clean um functions that destoy_mutexes and free_resources
 
     return (0); 
 }
